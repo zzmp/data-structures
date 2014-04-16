d270db346945fa20eb4fbb3f5779982120d5f484
zzmp <zmp@umich.edu>
2014-04-16 16:42:24 -0700

    Linked List to contains()

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,29 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+
+    if (list.head === null) {
+      list.head = list.tail = node;
+    } else {
+      list.tail.next = list.tail = node;
+    }
   };
 
   list.removeHead = function(){
+    var node = list.head;
+    if (node === null) {
+      return undefined;
+    } else if (node === list.tail) {
+      list.tail = null;
+    }
+    list.head = node.next;
+
+    return node;
   };
 
   list.contains = function(target, node){
+
   };
 
   return list;
