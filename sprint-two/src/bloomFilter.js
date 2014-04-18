575c652c7c304cd8acefd8913f7fe88ea6d26aaf
zzmp <zmp@umich.edu>
2014-04-18 16:21:36 -0700

    Bloom Filter implementation

---
+++ sprint-two/src/bloomFilter.js
@@ -0,0 +1,58 @@
+var BitArray = function (bound) {
+  if (bound > 31) {
+    throw 'err';
+  }
+
+  this._instance = ~(Math.pow(2, bound + 1) - 1);
+};
+
+BitArray.prototype.set = function (pos) {
+  this._instance |= Math.pow(2, pos);
+};
+
+BitArray.prototype.get = function (pos) {
+  return !!(this._instance & Math.pow(2, pos));
+};
+
+var BloomFilter = function (m, k) {
+  this._m = m;
+  this._k = k;
+
+  this._filter = new BitArray(this._m);
+
+  this._hashers = [];
+  for ( var i = 0; i < this._k; i++ ) {
+    this._hashers.push(this.makeHash(i));
+  }
+
+};
+
+BloomFilter.prototype.set = function(key) {
+  _.each(this._hashers, function(hasher) {
+    this._filter.set(hasher(key));
+  });
+};
+
+BloomFilter.prototype.has = function(key) {
+  return _.chain(this._hashers)
+    .map(function (hasher) {
+      return this._filter.get(hasher(key));
+    })
+    .every()
+    .value();
+};
+
+BloomFilter.prototype.makeHash = function (seed) {
+  return function (key) {
+    var hash = Math.pow(seed,seed);
+    for (var i = 0; i < key.length; i++) {
+      hash = (hash<<seed) + hash + key.charCodeAt(i);
+      hash = hash & hash; // Convert to 32bit integer
+      if (seed % 2) {
+        hash = ~hash;
+      }
+      hash = Math.abs(hash);
+    }
+    return hash % this._m;
+  };
+};
